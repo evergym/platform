32c800ee6f29c1dd45c8a600c89dfa2d9d07ecdb
Jim <wefactoryou@gmail.com>
2017-12-29 09:51:38 -0500

    update production.rb for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Evergym_#{Rails.env}"
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -85,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://blooming-brook-31200.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://blooming-brook-31200.herokuapp.com' }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
