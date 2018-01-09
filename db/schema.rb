74b7fbdef43ba131ff31761d19684d31fe8125d3
Jim <wefactoryou@gmail.com>
2018-01-09 14:09:30 -0800

    instand/request bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108223410) do
+ActiveRecord::Schema.define(version: 20180109192355) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "room_id"
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20180108223410) do
     t.datetime "end_date"
     t.integer  "price"
     t.integer  "total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "status",     default: 0
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -72,10 +73,11 @@ ActiveRecord::Schema.define(version: 20180108223410) do
     t.boolean  "active"
     t.integer  "price"
     t.integer  "user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "instant",       default: 1
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
