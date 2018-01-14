9978a26cbca84fbab63e84cb186fbc40ebea6e31
Jim <wefactoryou@gmail.com>
2018-01-14 08:37:07 -0800

    send email and sms for successful reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -54,6 +54,15 @@ class ReservationsController < ApplicationController
   end
   
   private
+    def send_sms(room, reservation)
+      @client = Twilio::REST::Client.new
+      @client.messages.create(
+        from: '+12017304620',
+        to: room.user.phone_number,
+        body: "#{reservation.user.first_name} #{reservation.user.last_name} booked '#{room.listing_name}'"
+      )
+    end
+  
     def charge(room, reservation)
       if !reservation.user.stripe_id.blank?
         customer = Stripe::Customer.retrieve(reservation.user.stripe_id)
@@ -70,6 +79,8 @@ class ReservationsController < ApplicationController
 
         if charge
           reservation.Approved!
+          ReservationMailer.send_email_to_guest(reservation.user, room).deliver_later
+          send_sms(room, reservation)
           flash[:notice] = "Reservation created successfully!"
         else
           reservation.Declined!
